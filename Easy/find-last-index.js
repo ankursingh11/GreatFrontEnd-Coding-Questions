f4c8fb6d31edb31d6ed84832fc6464332031ebf5
Ankur Singh <70473170+ankursingh11@users.noreply.github.com>
2024-06-02 14:50:00 +0530

    todays questions

---
+++ Easy/find-last-index.js
@@ -0,0 +1,27 @@
+
+// https://www.greatfrontend.com/questions/javascript/find-last-index
+
+/**
+ * This function returns the index of the last element in the array that satisfies the provided testing function.
+ * Otherwise, it returns -1.
+ *
+ * @param {Array} array - The array to search.
+ * @param {Function} predicate - The function invoked per iteration.
+ * @param {number} [fromIndex=array.length-1] - The index to start searching from.
+ * @returns The index of the found element, else -1.
+ */
+export default function findLastIndex(
+  array,
+  predicate,
+  fromIndex = array.length - 1,
+) {
+  
+  let length = array.length;
+  let startIndex = (fromIndex < 0) ? Math.max(fromIndex+length, 0) : Math.min(fromIndex, length-1);
+
+  for(let i = startIndex; i >= 0; i--){
+    if(predicate(array[i], i, array)) return i;
+  }
+
+  return -1;
+}
