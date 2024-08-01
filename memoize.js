018e71951ae9f97d0a2c9d6e71ac30f2279d3eee
Ankur Singh <70473170+ankursingh11@users.noreply.github.com>
2024-08-01 09:21:39 +0530

    daily questions

---
+++ memoize.js
@@ -0,0 +1,17 @@
+
+// https://www.greatfrontend.com/questions/javascript/memoize
+
+/**
+ * @param {Function} func
+ * @returns Function
+ */
+export default function memoize(func) {
+  const cache = new Map;
+  return function(arg){
+    if(cache.has(arg)) return cache.get(arg);
+
+    const result = func.call(this, arg);
+    cache.set(arg, result);
+    return result;
+  }
+}
